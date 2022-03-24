5a19ac374adaaec99f00e2f087b670873633f788
Oran C <oranbusiness@gmail.com>
2022-03-23 22:56:49 -0700

    add: missing shabang line to start.sh

--- pirateradio/start.sh
+++ pirateradio/start.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 find . -name 'morse_data.txt' |  entr -r cwwav -f 400 -w 20 -o /usr/src/app/PiFmAdv/src/public/hello_world.wav /usr/src/app/PiFmAdv/src/public/morse_data.txt &  
 /usr/src/app/PiFmAdv/src/pi_fm_adv --freq $RADIO_STATION_FREQ --mpx 30 --wait 0 --audio /usr/src/app/PiFmAdv/src/public/hello_world.wav
