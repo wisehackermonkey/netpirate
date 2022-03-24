bd2b10c79ba71a2984a95e28994942cd969216e0
Oran C <oranbusiness@gmail.com>
2022-03-23 22:45:15 -0700

    add start sh

---
+++ pirateradio/start.sh
@@ -0,0 +1,2 @@
+find . -name 'morse_data.txt' |  entr -r cwwav -f 400 -w 20 -o /usr/src/app/PiFmAdv/src/public/hello_world.wav /usr/src/app/PiFmAdv/src/public/morse_data.txt &  
+exec  /usr/src/app/PiFmAdv/src/pi_fm_adv --freq $RADIO_STATION_FREQ --mpx 30 --wait 0 --audio /usr/src/app/PiFmAdv/src/public/hello_world.wav"
