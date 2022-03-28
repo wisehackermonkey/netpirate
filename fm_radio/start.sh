736f4c92bb4139eb9a2c8baa3a0b3b6015969cb7
Oran C <oranbusiness@gmail.com>
2022-03-28 16:41:53 -0700

    add stuff

--- fm_radio/start.sh
+++ fm_radio/start.sh
@@ -2,7 +2,10 @@
 #!/bin/sh
 # pkill -l pi_fm_adv
 while true; do 
-    # pkill -l pi_fm_adv
-    # pkill /usr/src/app/PiFmAdv/src/pi_fm_adv
-    exec find . -name 'hello_world.wav' |  entr -r /usr/src/app/PiFmAdv/src/pi_fm_adv --freq $RADIO_STATION_FREQ --mpx 30 --gpio $GPIO_PIN --wait 0 --audio /public/hello_world.wav
+    ps ax 
+    ps ax |  awk '/src\/pi_fm_adv/ { print $1 }' 
+    ps ax |  awk '/src\/pi_fm_adv/ { print $1 }' | pkill -
+    # exec find . -name 'hello_world.wav' |  entr -r /usr/src/app/PiFmAdv/src/pi_fm_adv --freq $RADIO_STATION_FREQ --mpx 30 --gpio $GPIO_PIN --wait 1 --audio /public/hello_world.wav
+    /usr/src/app/PiFmAdv/src/pi_fm_adv --freq $RADIO_STATION_FREQ --mpx 30 --gpio $GPIO_PIN --wait 1 --audio /public/hello_world.wav
+    sleep 1
 done
