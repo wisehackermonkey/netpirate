ba217d7ed2e053ee3f0b3f1ec0827e64c95e2471
Oran C <oranbusiness@gmail.com>
2022-03-31 10:30:37 -0700

    fix: path issue with morse

--- fm_radio/start.sh
+++ fm_radio/start.sh
@@ -1,6 +1,6 @@
 
 #!/bin/sh
-/usr/src/app/PiFmAdv/src/pi_fm_adv --freq $RADIO_STATION_FREQ --mpx 30 --gpio $GPIO_PIN --wait 0 --audio /usr/src/app/PiFmAdv/src/public/hello_world.wav
+/usr/src/app/PiFmAdv/src/pi_fm_adv --freq $RADIO_STATION_FREQ --mpx 30 --gpio $GPIO_PIN --wait 0 --audio /usr/src/app/hello_world.wav
 while true; do 
     find . -name 'hello_world.wav' |  entr -r /usr/src/app/PiFmAdv/src/pi_fm_adv --freq $RADIO_STATION_FREQ --mpx 30 --gpio $GPIO_PIN --wait 0 --audio /usr/src/app/PiFmAdv/src/public/hello_world.wav
 done
